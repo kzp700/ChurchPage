2ba0e6f3a5f1e0bfa09d9af5fd7f0f01f7678ab2
Brandon Russell <kzp700@gmail.com>
2021-05-11 13:30:26 -0700

    Updated site logo. Added additional pages and updated backend routing for them. Updated nav bar to show new pages. Changed main page to fixed background with scrolling text.

--- app.js
+++ app.js
@@ -21,22 +21,30 @@ app.get("/", function(req, res){
     res.render("home");
   });
 
-  app.get("/messages", function(req, res) {
+  app.get("/new", function(req, res) {
+    res.render("new");
+  });
+
+app.get("/messages", function(req, res) {
     res.render("messages");
   });
 
-  app.get("/contact", function(req, res){
+app.get("/contact", function(req, res){
     res.render("contact");
   });
 
-  app.get("/give", function(req, res){
+  app.get("/about", function(req, res) {
+    res.render("about");
+  });
+
+app.get("/give", function(req, res){
     res.render("give");
   });
   
 
 
   
-  app.listen(3000, function() {
+app.listen(3000, function() {
     console.log("Server started on port 3000.");
   });
   
